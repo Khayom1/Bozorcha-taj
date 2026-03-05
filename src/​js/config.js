7d304dcf44eee11227f76e0d80902276d5e261b8
Veyrost <142344499+Khayom1@users.noreply.github.com>
2026-03-05 17:57:39 +0500

    Update config.js

--- src/​js/config.js
+++ src/​js/config.js
@@ -1,1 +1,12 @@
+// Танзимоти асосии лоиҳа
+const CONFIG = {
+    SUPABASE_URL: "https://seagtlaujnnhoyitgsdd.supabase.co",
+    SUPABASE_ANON_KEY: "sb_publishable_OlLChtOja0i8nIrPZuM_Og_vyfGjfsP",
+    APP_NAME: "Bozorcha",
+    APP_VERSION: "1.0.0"
+};
 
+// Агар лозим бошад, барои дастрасӣ дар дигар файлҳо
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = CONFIG;
+}
